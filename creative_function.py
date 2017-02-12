2cae2f094d5cc90aaa8672cede53cbf27e00762f
Johnny Chiu <johnnychiuchiu@gmail.com>
2017-02-12 15:32:08 +0800

    - change webClickScore function - update find_best_ad function using webClickScore - update analysis_column_generator with 'spent' column - create feature_importance function

--- creative_function.py
+++ creative_function.py
@@ -8,7 +8,7 @@
 
 
 def webClickScore(cpc, ctr, clicks):
-    scoreSum = (cpc - cpc.mean()) / (cpc.max() - cpc.min()) + (ctr - ctr.mean()) / (ctr.max() - ctr.min()) + (clicks - clicks.mean()) / (clicks.max() - clicks.min())
+    scoreSum =  (ctr - ctr.mean()) / (ctr.max() - ctr.min()) + (clicks - clicks.mean()) / (clicks.max() - clicks.min()) - (cpc - cpc.mean()) / (cpc.max() - cpc.min())
     return 80 + 32 * (scoreSum - scoreSum.mean()) / (scoreSum.max() - scoreSum.min())
 
 
@@ -22,12 +22,17 @@ def find_best_ad(df):
     dimension=['gender','age']
     impression_threshold=1000
     
-    temp=etungo_df.groupby(dimension+['ad_id'])[['impression','link_clicks']].sum().reset_index()
-    temp2=temp[temp.impression>impression_threshold]
-    temp3=temp2.assign(CTR=temp2.link_clicks/temp2.impression*1.0) 
-    idx= temp3.groupby(['gender','age'])['CTR'].transform(max) == temp3['CTR']
-    temp4=temp3[idx]
-    return temp4
+    temp=df.groupby(dimension+['ad_id'])[['impression','link_clicks','spent']].sum().reset_index()
+    temp=temp[temp.impression>impression_threshold]
+    temp=temp.assign(CTR=temp.link_clicks/temp.impression*1.0) 
+    temp=temp.assign(CPC=temp.spent/temp.link_clicks*1.0) 
+    temp.CPC=temp.CPC.replace(np.inf, np.nan)
+    temp.CPC=temp.CPC.replace(np.nan, temp.CPC.max())
+    temp['score']=webClickScore(temp.CPC, temp.CTR, temp.link_clicks)
+
+    idx=temp.groupby(['gender','age'])['score'].transform(max) ==temp['score']
+    temp2=temp[idx]
+    return temp2
 
 
 
@@ -91,15 +96,20 @@ def find_best_feature(analysis_df):
     dimension=['gender','age']
     impression_threshold=1000
     for name in analysis_df:
-        if name not in dimension+['impression','link_clicks']:
-            temp=analysis_df.groupby(dimension+[name])[['impression','link_clicks']].sum().reset_index()
-            temp2=temp[temp.impression>impression_threshold]
-            temp3=temp2.assign(CTR=temp2.link_clicks/temp2.impression*1.0) 
-            idx= temp3.groupby(dimension)['CTR'].transform(max) == temp3['CTR']
-            temp4=temp3[idx]
+        if name not in dimension+['impression','link_clicks','spent']:
+            temp=analysis_df.groupby(dimension+[name])[['impression','link_clicks','spent']].sum().reset_index()
+            temp=temp[temp.impression>impression_threshold]
+            temp=temp.assign(CTR=temp.link_clicks/temp.impression*1.0) 
+            temp=temp.assign(CPC=temp.spent/temp.link_clicks*1.0) 
+            temp.CPC=temp.CPC.replace(np.inf, np.nan)
+            temp.CPC=temp.CPC.replace(np.nan, temp.CPC.max())
+            temp['score']=webClickScore(temp.CPC, temp.CTR, temp.link_clicks)
+            
+            idx= temp.groupby(dimension)['score'].transform(max) == temp['score']
+            temp2=temp[idx]
         
-            temp_reshape=pd.melt(temp4,id_vars=dimension+['impression','link_clicks','CTR'])
-            temp_reshape=temp_reshape.loc[:,dimension+['variable','value','impression','link_clicks','CTR']]
+            temp_reshape=pd.melt(temp2,id_vars=dimension+['impression','link_clicks','spent','CTR','CPC','score'])
+            temp_reshape=temp_reshape.loc[:,dimension+['variable','value','impression','link_clicks','spent','CTR','CPC','score']]
         
             final_feature=pd.concat([final_feature, temp_reshape])
         
@@ -107,37 +117,70 @@ def find_best_feature(analysis_df):
 
     return final_feature
 
+############################################ feature_importance ############################################ 
+##### goal: find the feature importance for each dimension
+##### input
+#####       analysis_df: a table with all columns that are ready for analysis
+##### output 
+#####       a dataframe that provides the feature importance ranking for the targeted dimension, such as {gender, age}
 
-find_best_feature<- function(analysis_df){
-  library(reshape)
-  final_feature=data.frame()
-  dimension<-c('gender','age')
-  impression_threshold=1000
+def feature_importance(analysis_df):
+    import copy
+    feature=copy.deepcopy(analysis_df)
+    feature=feature.drop(['impression','link_clicks','spent'], axis=1)
+    importance_df=pd.DataFrame({'feature':feature.columns,
+                            'max_impression':0,'max_click':0,'max_spent':0,'max_ctr':0,'max_cpc':0,'max_score':0,
+                            'min_impression':0,'min_click':0,'min_spent':0,'min_ctr':0,'min_cpc':0,'min_score':0,
+                            'importance':0})
+    impression_threshold=1000
+
+    for name in feature.columns:
+        print name
+        temp=analysis_df.groupby([name])[['impression','link_clicks','spent']].sum().reset_index()
+        temp=temp[temp.impression>impression_threshold]
+        temp=temp.assign(CTR=temp.link_clicks/temp.impression*1.0) 
+        temp=temp.assign(CPC=temp.spent/temp.link_clicks*1.0) 
+        temp.CPC=temp.CPC.replace(np.inf, np.nan)
+        temp.CPC=temp.CPC.replace(np.nan, temp.CPC.max())
+        temp['score']=webClickScore(temp.CPC, temp.CTR, temp.link_clicks)
+    
+        if temp.shape[0]==1:
+            importance_df=importance_df[importance_df.feature!=name]
+        else:
+#            max_score= temp.score.max()
+#            min_score= temp.score.min()
+#            importance_df.importance[importance_df.feature==name]= max_score-min_score
+#            importance_df.max_score[importance_df.feature==name]= max_score
+#            importance_df.min_score[importance_df.feature==name]= min_score
+#            importance_df.max_impression[importance_df.feature==name]= temp.impression[temp.score==max_score].values
+#            importance_df.min_impression[importance_df.feature==name]= temp.impression[temp.score==min_score].values
+#            importance_df.max_click[importance_df.feature==name]= temp.link_clicks[temp.score==max_score].values
+#            importance_df.min_click[importance_df.feature==name]= temp.link_clicks[temp.score==min_score].values
+#            importance_df.max_spent[importance_df.feature==name]= temp.spent[temp.score==max_score].values
+#            importance_df.min_spent[importance_df.feature==name]= temp.spent[temp.score==min_score].values
+#            importance_df.max_ctr[importance_df.feature==name]= temp.CTR[temp.score==max_score].values
+#            importance_df.min_ctr[importance_df.feature==name]= temp.CTR[temp.score==min_score].values
+#            importance_df.max_cpc[importance_df.feature==name]= temp.CPC[temp.score==max_score].values
+#            importance_df.min_cpc[importance_df.feature==name]= temp.CPC[temp.score==min_score].values
+            
+            max_score= temp.CTR.max()
+            min_score= temp.CTR.min()
+            importance_df.importance[importance_df.feature==name]= temp.score[temp.CTR==max_score].values-temp.score[temp.CTR==min_score].values
+            importance_df.max_score[importance_df.feature==name]= temp.score[temp.CTR==max_score].values
+            importance_df.min_score[importance_df.feature==name]= temp.score[temp.CTR==min_score].values
+            importance_df.max_impression[importance_df.feature==name]= temp.impression[temp.CTR==max_score].values
+            importance_df.min_impression[importance_df.feature==name]= temp.impression[temp.CTR==min_score].values
+            importance_df.max_click[importance_df.feature==name]= temp.link_clicks[temp.CTR==max_score].values
+            importance_df.min_click[importance_df.feature==name]= temp.link_clicks[temp.CTR==min_score].values
+            importance_df.max_spent[importance_df.feature==name]= temp.spent[temp.CTR==max_score].values
+            importance_df.min_spent[importance_df.feature==name]= temp.spent[temp.CTR==min_score].values
+            importance_df.max_ctr[importance_df.feature==name]= temp.CTR[temp.CTR==max_score].values
+            importance_df.min_ctr[importance_df.feature==name]= temp.CTR[temp.CTR==min_score].values
+            importance_df.max_cpc[importance_df.feature==name]= temp.CPC[temp.CTR==max_score].values
+            importance_df.min_cpc[importance_df.feature==name]= temp.CPC[temp.CTR==min_score].values
+    
+    importance_df=importance_df.sort_values(by=['importance'], ascending=False) 
+    importance_df['percentage']=  importance_df.importance / importance_df.importance.sum()
+       
+    return importance_df    
   
-  for(name in colnames(analysis_df)){  # 
-    if(!(name %in% c('gender','age','impression','link_clicks'))){
-      grp_cols <- c(dimension, name) # Columns you want to group by
-      dots <- lapply(grp_cols, as.symbol)# Convert character vector to list of symbols
-      dots2 <- lapply(dimension, as.symbol)# Convert character vector to list of symbols
-      
-      temp_feature_df<-analysis_df %>% group_by_(.dots=dots) %>% 
-        dplyr::summarise(total_impression=sum(impression), total_click=sum(link_clicks)) %>% 
-        filter(total_impression > impression_threshold) %>%
-        mutate(CTR=total_click/total_impression) %>%
-        group_by_(.dots=dots2) %>% slice(which.max(CTR)) 
-      
-      temp_reshape <- melt(temp_feature_df, id=c(dimension,"total_impression",'total_click','CTR')) %>% 
-        select(gender, age, variable, value, total_impression, total_click, CTR)
-      
-      final_feature<- rbind(final_feature,temp_reshape) 
-    }
-  }
-  
-  final_feature<- final_feature %>% arrange(gender, age)
-  return(final_feature)
-}
-
-
-
-
-
