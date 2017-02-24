f4f3c3ad560bbbc0c6cfba974d78558acaf6d1b5
Johnny Chiu <johnnychiuchiu@gmail.com>
2017-02-24 23:26:16 +0800

    -update script for label feature analysis

--- creative_analysis.py
+++ creative_analysis.py
@@ -9,14 +9,17 @@ from initDataFrame import *
 
 
 
+
 ######################
 ##### 讀檔案進來 ##### 
 ######################
+insightPath='../facebook_ad_report_ver3/insightData/age_gender_json_0217'
+googlevisionPath='../facebook_ad_report_ver3/insightData/with-x-y-google-vision_0217.json'
 
-mydata = initDataFrame('../facebook_ad_report_ver3/insightData/age_gender_json_0217',
-                '../facebook_ad_report_ver3/insightData/with-x-y-google-vision_0217.json')
+mydata = initDataFrame(insightPath, googlevisionPath)
 mydata['account_name'] = mydata['account_name'].str.strip()
 
+gv_df_label = gv_data_reader(googlevisionPath)
 
 
 ######################################################################
@@ -28,6 +31,8 @@ etungo_df=mydata[mydata.account_id == 1618494121752703]
 etungo_df=brand_column_generator(etungo_df,'大同|etungo')
 #pd.isnull(etungo_df).any(axis=0)
 etungo_df=metric_generator(etungo_df)
+
+etungo_df=image_label_generator(etungo_df,100,gv_df_label)
 
 
 
@@ -42,6 +47,9 @@ best_ad=find_best_ad(etungo_df)
 
 
 
+
+
+
 ####################################################
 #####find the best ad and feature for each group ###
 ####################################################
@@ -49,19 +57,28 @@ best_ad=find_best_ad(etungo_df)
 best_ad_gender=find_best_ad_by_segment(etungo_df,'gender')
 best_ad_age=find_best_ad_by_segment(etungo_df,'age')
 
-#find_ad_feature(etungo_df, [6055843154405])
+a=find_ad_feature(etungo_df, gv_df_label, [6055843154405])
 #best_ad_gender_feature=find_ad_feature(etungo_df, best_ad_gender.ad_id.unique().tolist())
 #best_ad_age_feature=find_ad_feature(etungo_df, best_ad_age.ad_id.unique().tolist())
 
 
-#################################
-#####filter analyzing columns ###
-#################################
+
+##############################################
+#####filter and generate analyzing columns ###
+##############################################
 ### ad related: title_brand, sub_title_brand, ad_content_brand, title_length_interval, sub_title_length_interval, ad_content_length_interval, call_to_action
 ### ad performance related: age, gender, impression, click
 ### google vision related: faceCount, majorColor, textInImage, logoInImage, adult, medical, spoof, violence, image category
 
-etungo_df_analysis=column_selector(etungo_df)
+etungo_df_analysis=column_selector(etungo_df,gv_df_label)
+#etungo_df_analysis.to_csv('etungo_df_analysis.csv', sep=',', encoding='utf-8')
+#pd.isnull(etungo_df_analysis).any(axis=0)
+#etungo_df_analysis.columns
+
+
+
+
+
 
 
 
@@ -69,18 +86,24 @@ etungo_df_analysis=column_selector(etungo_df)
 #####find the best feature for each group ###
 #############################################
 #etungo_df_analysis=metric_generator(etungo_df_analysis)
-#final_feature=find_feature(etungo_df_analysis,'gender')
+final_feature=find_feature(etungo_df_analysis,'age','18-24',etungo_df,gv_df_label)
 
 #final_feature.to_csv('final_feature.csv', sep=',', encoding='utf-8')
 #etungo_df_analysis.head(10) 
 #test=final_feature[final_feature.variable=='title_brand']
 #test.head(20)
 
+final_label=find_label_feature(etungo_df[etungo_df.age=='18-24'],gv_df_label,100,'age')
+
+
+
 ###################################################
 #####find the feature importance for each group ###
 ###################################################
 
-#importance_df=find_importance(etungo_df_analysis)
+importance_df=find_importance(etungo_df_analysis,'age','18-24',etungo_df,gv_df_label)
+
+
 
 
 
@@ -92,52 +115,18 @@ etungo_df_analysis=column_selector(etungo_df)
 #print_full(feature_and_importance_65)
 #a=find_ad_feature(etungo_df, [6050712185805])
 #print_full(a)
+  
 
-feature_and_importance_female = find_feature_and_importance(etungo_df_analysis,'gender','female')
-feature_and_importance_male = find_feature_and_importance(etungo_df_analysis,'gender','male')
-feature_and_importance_unknown = find_feature_and_importance(etungo_df_analysis,'gender','unknown')
+feature_and_importance_female = find_feature_and_importance(etungo_df_analysis,'gender','female',etungo_df,gv_df_label)
+feature_and_importance_male = find_feature_and_importance(etungo_df_analysis,'gender','male',etungo_df,gv_df_label)
+feature_and_importance_unknown = find_feature_and_importance(etungo_df_analysis,'gender','unknown',etungo_df,gv_df_label)
 
-feature_and_importance_1824 = find_feature_and_importance(etungo_df_analysis,'age','18-24')
-feature_and_importance_2534 = find_feature_and_importance(etungo_df_analysis,'age','25-34')
-feature_and_importance_3544 = find_feature_and_importance(etungo_df_analysis,'age','35-44')
-feature_and_importance_4554 = find_feature_and_importance(etungo_df_analysis,'age','45-54')
-feature_and_importance_5564 = find_feature_and_importance(etungo_df_analysis,'age','55-64')
-feature_and_importance_65 = find_feature_and_importance(etungo_df_analysis,'age','65+')
-
-campaign_id='6060850543605,6059224129805,6059889803605,6059892762805,6059893654805,6057193158805,6055407354005,6053312447405,6051265864405,6051266212805,6049374949605,6035884102605,6035882865005,6034201296805,6034192331405,6033466101805,6032523746205,6033118372005,6033118372205,6032205731205,6032523746005,6032205730605,6031518441405,6031616546205,6031518442005,6031518441605,6031518441205,6031518441005,6032906211805,6032906682605,6032907243605,6032261374205,6032627603405,6032260651005,6032626832005,6032321919605,6032411410005,6032321128805,6032200009405,6031742821405,6031796276605,6031722773205'
-campaign_ids=campaign_id.split(',')
-campaign_ids = map(int, campaign_ids)
-    
-campaign_data=mydata[mydata.campaign_id.isin(campaign_ids)]
-campaign_data=metric_generator(campaign_data)
-campaign_data_analysis=column_selector(campaign_data)
-    
-feature_and_importance_female = find_feature_and_importance(campaign_data_analysis,'gender','female')
-feature_and_importance_male = find_feature_and_importance(campaign_data_analysis,'gender','male')
-feature_and_importance_unknown = find_feature_and_importance(campaign_data_analysis,'gender','unknown')
-
-feature_and_importance_1824 = find_feature_and_importance(campaign_data_analysis,'age','18-24')
-feature_and_importance_2534 = find_feature_and_importance(campaign_data_analysis,'age','25-34')
-feature_and_importance_3544 = find_feature_and_importance(campaign_data_analysis,'age','35-44')
-feature_and_importance_4554 = find_feature_and_importance(campaign_data_analysis,'age','45-54')
-feature_and_importance_5564 = find_feature_and_importance(campaign_data_analysis,'age','55-64')
-feature_and_importance_65 = find_feature_and_importance(campaign_data_analysis,'age','65+')
-    
-df_list=[feature_and_importance_female,feature_and_importance_male,feature_and_importance_unknown,
-feature_and_importance_1824,feature_and_importance_2534,feature_and_importance_3544,
-feature_and_importance_4554,feature_and_importance_5564,feature_and_importance_65]
-
-result_df = pd.DataFrame(columns=['segment','value','recommend'])
-    
-for index,df in enumerate(df_list):
-    temp=df[['feature','value','percentage']]
-    temp=temp[temp.percentage.notnull()]
-    temp.set_index(temp.feature, inplace = True)
-    del temp['feature']
-    temp['priority']=range(1, temp.shape[0]+1)
-    temp_dict=temp.to_dict(orient='index')
-    
-    result_df.loc[index]=pd.Series({'segment':df.columns[0],'value':df.iloc[0][0],'recommend':temp_dict})
+feature_and_importance_1824 = find_feature_and_importance(etungo_df_analysis,'age','18-24',etungo_df,gv_df_label)
+feature_and_importance_2534 = find_feature_and_importance(etungo_df_analysis,'age','25-34',etungo_df,gv_df_label)
+feature_and_importance_3544 = find_feature_and_importance(etungo_df_analysis,'age','35-44',etungo_df,gv_df_label)
+feature_and_importance_4554 = find_feature_and_importance(etungo_df_analysis,'age','45-54',etungo_df,gv_df_label)
+feature_and_importance_5564 = find_feature_and_importance(etungo_df_analysis,'age','55-64',etungo_df,gv_df_label)
+feature_and_importance_65 = find_feature_and_importance(etungo_df_analysis,'age','65+',etungo_df,gv_df_label)
 
 
 
@@ -229,6 +218,7 @@ best_ad=find_best_ad(mamaway_df)
 
 best_ad_gender=find_best_ad_by_segment(mamaway_df,'gender')
 best_ad_age=find_best_ad_by_segment(mamaway_df,'age')
+
 
 
 
